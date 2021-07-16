ef023ace83d7ced449fba27d9b003640c2fc5ce7
Alejandro <acuellarh@gmail.com>
2021-07-16 18:02:46 -0500

    Add & config of Express, Mongoose with environment variables + initial command to install hbs express

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require('express')
+const app = express()
+
+
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+
+app.listen(process.env.PORT || 3000, console.log(`runnig in port ${process.env.PORT || 3000}`))
