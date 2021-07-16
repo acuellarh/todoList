abfd002f29f1298db46089deefb2803c4fee7208
Alejandro <acuellarh@gmail.com>
2021-07-16 18:12:50 -0500

    require the DB

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 
+// mongo db connection
+require('./config/dbConfig');
 
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+
+
+
 
 
 app.listen(process.env.PORT || 3000, console.log(`runnig in port ${process.env.PORT || 3000}`))
